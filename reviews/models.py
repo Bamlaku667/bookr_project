07735cd78f9712b08f51d2e3c18a75e175e2e28c
Bamlak <bamlakhiruya@gmail.com>
2023-03-06 01:02:19 -1000

    adding a relation between each models

--- reviews/models.py
+++ reviews/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib import auth
 
 # Create your models here.
 class Publisher(models.Model):
@@ -10,21 +11,6 @@ class Publisher(models.Model):
     website = models.URLField(help_text = "the company's website")
     email = models.EmailField(\
         help_text="The publisher's email address.")
-
-
-class Book(models.Model):
-    """A publishe book """
-    title = models.TextField(\
-        max_length=70,\
-            help_text= "The title of the book.")
-    
-    publication_date = models.DateField(\
-        verbose_name=\
-            "Date of the book was published")
-    
-    isbn = models.CharField(\
-        max_length=20, \
-            verbose_name="ISBN number of the book.")
 
 
 class Contributor(models.Model):
@@ -40,7 +26,47 @@ class Contributor(models.Model):
     email = models.EmailField(\
         help_text="The contact email of the contributor.")
 
+class Book(models.Model):
+    """A publishe book """
+    title = models.TextField(\
+        max_length=70,\
+            help_text= "The title of the book.")
+    
+    publication_date = models.DateField(\
+        verbose_name=\
+            "Date of the book was published")
+    
+    isbn = models.CharField(\
+        max_length=20, \
+            verbose_name="ISBN number of the book."), 
 
+    publisher = models.ForeignKey(\
+        Publisher, on_delete= models.CASCADE)
+    
+    contributor = models.ManyToManyField('Contributor', through="BookContributor")
+
+
+
+
+class BookContributor(models.Model):
+    class ContribtionRole(models.TextChoices):
+        AUTHOR = "AUTHOR", "Author"
+        CO_AUTHOR = "CO_AUTHOR", "Co-Author"
+        EDITOR = "EDITOR", "Editor"
+    
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    contributor = models.ForeignKey(Contributor, on_delete= models.CASCADE)
+
+    role = models.CharField(\
+        verbose_name="The role of the contributor for the book", \
+            choices= ContribtionRole.choices, max_length= 20)
 class Reviews(models.Model):
-    ...
+    content = models.TextField(help_text="The Review text.")
+    rating = models.IntegerField(help_text="The rating the reviewer has given")
+    date_created = models.DateTimeField(auto_now=True, help_text="The date and time the review was created")
+    date_edited = models.DateTimeField(null=True, help_text= " The date and time the review was last edited")
+    creator = models.ForeignKey(auth.get_user_model(), on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete= models.CASCADE, help_text="The book that this review is for.")
+
 
