f69adf6ba8ea9a1f44ff2afd41725fa0bbed0b93
Bamlak <bamlakhiruya@gmail.com>
2023-03-10 11:56:22 -1000

    adding a book_list page view and template

--- reviews/views.py
+++ reviews/views.py
@@ -1,16 +1,30 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book
+from django.views.generic import TemplateView
+from .utils import average_rating
 from django.shortcuts import render
-# Create your views here.
 
-def index(request):
-    name = "bamlaku"
+
+def book_list(request):
+    books = Book.objects.all()
+    book_list = []
+    for book in books:
+        reviews = book.review_set.all()
+        if reviews:
+            book_rating = average_rating([review.rating for review in reviews])
+            number_of_reviews = len(reviews)
+        else:
+            book_rating = None
+            number_of_reviews = 0
+
+        book_list.append({'book': book,
+                          'book_rating': book_rating,
+                          'number_of_reviews': number_of_reviews})
+        
     context = {
-        'name': name
-    }    
-    return render(request, "base.html", context= context) #there are other options other than render 
+        'book_list': book_list
+    }
 
-def search(request):
-    name = request.GET.get("search") or "<html/>"
-    context = {'name': name}
-    return render(request, "search.html", context= context)
+    return render(request, 'reviews/book_list.html', context = context)
+
+
